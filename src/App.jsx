53b8f827f458563f2c7269b6e58cb278d83f9efa
swordmaster2005 <soulmaster9794@gmail.com>
2023-08-22 22:12:22 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,24 @@ import SpeakerBoss from './pages/Spekers/SpeakerBoss'
 import EarPhonesBoss from './pages/EarPhones/EarPhonesBoss'
 import RootLayout from './layout/RootLayout'
 import HomeLayout from './layout/HomeLayout'
+import HeadPhonesLayout from './layout/HeadPhonesLayout'
+import HeadPhones1 from './details/HeadPhones1'
 
 
 const App = () => {
   const routes = createBrowserRouter(
     createRoutesFromElements(
-      <Route  element={<RootLayout/>}>
+       <Route  element={<RootLayout/>}>
         <Route path='/' element={<HomeLayout/>}>
             <Route index element={ <HomeBoss/>}/>
-            <Route path='headphones' element={ <HeadPhonesBoss/>}/>
+            <Route path='headphones' element={ <HeadPhones1/>}/>
             <Route path='speakers' element={ <SpeakerBoss/>}/>
             <Route path='earphones' element={ <EarPhonesBoss/>}/>
         </Route>
+        {/* <Route path='headphones' element={<HeadPhonesLayout/>}>
+            <Route index element={ <HeadPhonesBoss/>}/>
+            <Route path='headphones1' element={ <HeadPhones1/>}/>
+        </Route> */}
         
       </Route>
     )
